d9d7d60f1c62b1722e10ffec9119bbdce6ad2114
Rodrigo Godoy Méndez <rjgodoy@me.com>
2016-04-25 06:57:56 -0300

    Initial Commit

---
+++ Scale Entel/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Scale Entel
+//
+//  Created by Rodrigo Godoy Méndez on 4/25/16.
+//  Copyright © 2016 Yoyin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Scale EntelTests/Scale_EntelTests.swift
@@ -0,0 +1,36 @@
+//
+//  Scale_EntelTests.swift
+//  Scale EntelTests
+//
+//  Created by Rodrigo Godoy Méndez on 4/25/16.
+//  Copyright © 2016 Yoyin. All rights reserved.
+//
+
+import XCTest
+@testable import Scale_Entel
+
+class Scale_EntelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
